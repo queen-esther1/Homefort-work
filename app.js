1baa80718b66af15a0d8dfc9d902508e75bd7ac0
unknown <akpanqueenesther1@gmail.com>
2024-11-21 12:05:49 +0100

    my work on javascript

---
+++ app.js
@@ -0,0 +1,70 @@
+console.log('happy programming');
+console.log("the best part of learning is javascript");
+console.log("my line manager is mr. James");
+
+// variable declaration
+var middleName = "esther"
+console.log(middleName);
+
+let myNum = 25;
+console.log(25);
+
+// float - are decimal numbers( 5.7, 7.6)
+let myDecimal = 8.9;
+console.log(myDecimal);
+
+// boolean- consist of true or false values
+const isSunny = true;
+
+const isRaining = false;
+
+console.log("is it sunny?", isSunny);
+console.log("is it raining?", isRaining);
+
+// operators are used to perform different types of mathematical and logical simple task
+// operands are the data,the expressions or values, on which they act or work.
+// the arithmetic operations
+// the assignment operator (=) assigns values
+let alphaX = 90;
+console.log(alphaX);
+
+let alphaG = 5;
+console.log(alphaG);
+
+let result = alphaX % alphaG;
+console.log(result);
+
+// the multiplication operator (*) multiplies number
+let alphaF = 21;
+console.log(alphaF);
+
+let alphaS = 70;
+console.log(alphaS);
+
+let Answer = alphaF * alphaS;
+console.log(Answer);
+
+// the addition operator (+) adds number
+let text1 = "HOME";
+let text2 = "FORT";
+
+let word = text1 + text2;
+console.log(word);
+
+// Example2
+let sub = 8;
+let way = 10;
+
+let subWay = sub + way;
+console.log(subWay);
+
+// The + Operator
+//Adding a number and a string, returns a string.
+
+let y = "5";
+let u = 5
+let m = y + u
+console.log(m);
+
+let z = "Hello" + 5;
+console.log(z);
